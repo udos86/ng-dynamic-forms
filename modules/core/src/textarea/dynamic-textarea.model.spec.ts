c27f66aa9c6ba61f733d1cfc8d6b1a0e73f4b726
Udo Schöfer <developer@udos86.de>
2016-06-12 14:41:21 +0200

    unit test for DynamicTextAreaModel extended

--- modules/core/src/textarea/dynamic-textarea.model.spec.ts
+++ modules/core/src/textarea/dynamic-textarea.model.spec.ts
@@ -1,4 +1,8 @@
-import {DYNAMIC_FORM_CONTROL_TYPE_TEXTAREA, DynamicTextAreaModel} from "./dynamic-textarea.model";
+import {
+    DYNAMIC_FORM_CONTROL_TYPE_TEXTAREA, 
+    DYNAMIC_FORM_TEXTAREA_WRAP_SOFT,
+    DynamicTextAreaModel
+} from "./dynamic-textarea.model";
 
 describe("DynamicTextAreaModel test suite", () => {
 
@@ -14,6 +18,24 @@ describe("DynamicTextAreaModel test suite", () => {
 
             expect(defaultObject.type).toBeDefined();
             expect(defaultObject.type).toEqual(DYNAMIC_FORM_CONTROL_TYPE_TEXTAREA);
+        });
+
+        it("tests if correct default cols property is set", () => {
+
+            expect(defaultObject.cols).toBeDefined();
+            expect(defaultObject.cols).toBe(20);
+        });
+
+        it("tests if correct default rows property is set", () => {
+
+            expect(defaultObject.rows).toBeDefined();
+            expect(defaultObject.rows).toBe(2);
+        });
+
+        it("tests if correct default wrap property is set", () => {
+
+            expect(defaultObject.wrap).toBeDefined();
+            expect(defaultObject.wrap).toEqual(DYNAMIC_FORM_TEXTAREA_WRAP_SOFT);
         });
         
     });
