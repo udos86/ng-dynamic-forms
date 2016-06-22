7d080d4b29c9e6641ebaba1c202ed0490ed278a9
Udo Schöfer <developer@udos86.de>
2016-06-22 20:15:28 +0200

    Radio Groups ready in ui-basic and ui-bootstrap

--- modules/ui-basic/src/dynamic-form-basic.component.ts
+++ modules/ui-basic/src/dynamic-form-basic.component.ts
@@ -19,7 +19,7 @@ export class DynamicFormBasicComponent extends DynamicFormControlComponent {
     @Input() model: DynamicFormControlModel<any>;
     @Input() form: FormGroup;
 
-    incompatibilities: Array<string> = [DYNAMIC_FORM_CONTROL_TYPE_RADIO_GROUP];
+    incompatibilities: Array<string> = [/*DYNAMIC_FORM_CONTROL_TYPE_RADIO_GROUP*/];
     type: string = DYNAMIC_FORM_UI_BASIC;
 
     constructor() {
