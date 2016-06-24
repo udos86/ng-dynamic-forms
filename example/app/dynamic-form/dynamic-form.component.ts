55c9f6c09b3d54c3e01ebfe99af16676664c6db7
Udo Schöfer <developer@udos86.de>
2016-06-24 22:08:13 +0200

    ui-foundation module created

--- example/app/dynamic-form/dynamic-form.component.ts
+++ example/app/dynamic-form/dynamic-form.component.ts
@@ -2,10 +2,12 @@ import {Component} from "@angular/core";
 import {BootstrapExampleComponent} from "./bootstrap/bootstrap-example.component";
 import {MaterialExampleComponent} from "./material/material-example.component";
 import {BasicExampleComponent} from "./basic/basic-example.component";
+import {FoundationExampleComponent} from "./foundation/foundation-example.component";
 
 @Component({
 
-    directives: [BasicExampleComponent, BootstrapExampleComponent, MaterialExampleComponent],
+    directives: [BasicExampleComponent, BootstrapExampleComponent, FoundationExampleComponent, 
+        MaterialExampleComponent],
     moduleId: module.id,
     selector: "dynamic-form",
     templateUrl: "./dynamic-form.component.html",
---
+++ example/app/dynamic-form/foundation/foundation-example.model.ts
@@ -0,0 +1,136 @@
+import {
+    DynamicFormModel,
+    DynamicCheckboxModel,
+    DynamicInputModel,
+    DynamicSelectModel,
+    DynamicRadioGroupModel,
+    DynamicTextAreaModel,
+} from "@ng2-dynamic-forms/core";
+
+export const FOUNDATION_EXAMPLE_MODEL: DynamicFormModel = new DynamicFormModel([
+
+    new DynamicSelectModel<string>(
+        {
+            id: "bootstrapSelect",
+            label: {
+                text: "Example Select"
+            },
+            options: [
+                {
+                    text: "Option 1",
+                    value: "option-1",
+                },
+                {
+                    disabled: true,
+                    text: "Option 2",
+                    value: "option-2"
+                },
+                {
+                    text: "Option 3",
+                    value: "option-3"
+                },
+                {
+                    text: "Option 4",
+                    value: "option-4"
+                }
+            ],
+            value: "option-3"
+        },
+        {
+            grid: {
+                control: "col-sm-9",
+                label: "col-sm-3"
+            },
+            label: "control-label"
+        }
+    ),
+
+    new DynamicInputModel(
+        {
+            help: "Just a sample help text",
+            id: "bootstrapInput",
+            label: {
+                text: "Example Input"
+            },
+            maxLength: 51,
+            placeholder: "example input",
+            prefix: "Prefix",
+            suffix: "Suffix"
+        },
+        {
+            grid: {
+                control: "col-sm-9",
+                label: "col-sm-3"
+            },
+            label: "control-label"
+        }
+    ),
+
+    new DynamicRadioGroupModel<string>(
+        {
+            id: "bootstrapRadioGroup",
+            label: {
+                text: "Example Radio Group"
+            },
+            options: [
+                {
+                    text: "Option 1",
+                    value: "option-1",
+                },
+                {
+                    disabled: true,
+                    text: "Option 2",
+                    value: "option-2"
+                },
+                {
+                    text: "Option 3",
+                    value: "option-3"
+                },
+                {
+                    text: "Option 4",
+                    value: "option-4"
+                }
+            ],
+            value: "option-3"
+        },
+        {
+            grid: {
+                control: "col-sm-9",
+                label: "col-sm-3"
+            },
+            label: "control-label"
+        }
+    ),
+
+    new DynamicTextAreaModel(
+        {
+            id: "bootstrapTextArea",
+            label: {
+                text: "Example Textarea"
+            },
+            rows: 5,
+            placeholder: "example Textarea",
+        },
+        {
+            grid: {
+                control: "col-sm-9",
+                label: "col-sm-3"
+            },
+            label: "control-label"
+        }
+    ),
+
+    new DynamicCheckboxModel(
+        {
+            id: "bootstrapCheckbox",
+            label: {
+                text: "I do agree"
+            }
+        },
+        {
+            grid: {
+                control: "col-sm-offset-3 col-sm-9"
+            }
+        }
+    )
+]);
