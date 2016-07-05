9f6a1f1732cfa02d36e68abb46c5e9138ea695e7
Udo Schöfer <developer@udos86.de>
2016-07-05 23:14:21 +0200

    missing disabled attributes in templates added

--- example/app/basic/basic-example.component.e2e.spec.ts
+++ example/app/basic/basic-example.component.e2e.spec.ts
@@ -3,56 +3,30 @@ import {BASIC_EXAMPLE_MODEL} from "./basic-example.model";
 
 describe("BasicExampleComponent", () => {
 
-    let elements = {
-
-        checkbox: null,
-        checkboxGroup: null,
-        input: null,
-        radioGroup: null,
-        select: null,
-        textarea: null
-    };
-    
-    let models = {
-
-        checkbox: BASIC_EXAMPLE_MODEL.items[5],
-        checkboxGroup: BASIC_EXAMPLE_MODEL.items[2],
-        input: BASIC_EXAMPLE_MODEL.items[1],
-        radioGroup: BASIC_EXAMPLE_MODEL.items[3],
-        select: BASIC_EXAMPLE_MODEL.items[0],
-        textarea: BASIC_EXAMPLE_MODEL.items[4]
-    };
-    
-    beforeEach( () => {
+    beforeEach(() => {
 
         browser.get("./example-basic");
-
-        elements.checkbox = element(by.id("basicCheckbox"));
-        elements.checkboxGroup = element(by.id("basixCheckboxGroup"));
-        elements.input = element(by.id("basicInput"));
-        elements.radioGroup = element(by.id("basicRadioGroup"));
-        elements.select = element(by.id("basicSelect"));
-        elements.textarea = element(by.id("basicTextArea"));
     });
 
-    it("tests if all form controls do exist in DOM", () => {
+    it("tests if all form controls are rendered correctly", () => {
 
-        expect(elements.checkbox).toBeDefined();
-        expect(elements.checkboxGroup).toBeDefined();
-        expect(elements.input).toBeDefined();
-        expect(elements.radioGroup).toBeDefined();
-        expect(elements.select).toBeDefined();
-        expect(elements.textarea).toBeDefined();
-    });
+        BASIC_EXAMPLE_MODEL.items.forEach(model => {
 
-    it("tests if all form controls have correct name attribute", () => {
-        
-        elements.checkbox.getAttribute("name").then(name => expect(name).toEqual(models.checkbox.name));
-        elements.checkboxGroup.getAttribute("name").then(name => expect(name).toEqual(name));
-        elements.input.getAttribute("name").then(name => expect(name).toEqual(models.input.name));
-        elements.radioGroup.getAttribute("name").then(name => expect(name).toEqual(models.radioGroup.name));
-        elements.select.getAttribute("name").then(name => expect(name).toEqual(models.select.name));
-        elements.textarea.getAttribute("name").then(name => expect(name).toEqual(models.textarea.name));
+            let elm = element(by.id(model.id));
+
+            expect(elm).toBeDefined();
+            
+            elm.getAttribute("id").then(attr => expect(attr).toEqual(model.id));
+            elm.getAttribute("name").then(attr => expect(attr).toEqual(model.name));
+            elm.getAttribute("disabled").then(attr => {
+
+                if (model.disabled === true) {
+                    expect(attr).toBeTruthy();
+
+                } else {
+                    expect(attr).toBeNull();
+                }
+            });
+        });
     });
-    
 });
