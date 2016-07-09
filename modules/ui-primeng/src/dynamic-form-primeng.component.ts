c24d8aa87e3019a5b1869be775245ed7a36241c4
Udo Schöfer <developer@udos86.de>
2016-07-09 18:05:27 +0200

    PrimeNG Directives imported

--- modules/ui-primeng/src/dynamic-form-primeng.component.ts
+++ modules/ui-primeng/src/dynamic-form-primeng.component.ts
@@ -2,12 +2,13 @@ import {Component} from "@angular/core";
 import {Input} from "@angular/core";
 import {REACTIVE_FORM_DIRECTIVES, FormGroup} from "@angular/forms";
 import {DynamicFormControlComponent, DynamicFormControlModel} from "@ng2-dynamic-forms/core";
+import {Checkbox, InputText, InputTextarea, RadioButton, Spinner} from "primeng/primeng";
 
 export const DYNAMIC_FORM_UI_PRIME_NG = "PRIME_NG";
 
 @Component({
 
-    directives: [REACTIVE_FORM_DIRECTIVES],
+    directives: [REACTIVE_FORM_DIRECTIVES, Checkbox, InputText, InputTextarea, RadioButton, Spinner],
     moduleId: module.id,
     selector: "dynamic-form-primeng-control",
     templateUrl: "./dynamic-form-primeng.component.html"
