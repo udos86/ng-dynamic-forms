107225541d4db62f30a9711a7c1e5c232224e55b
Udo Schöfer <developer@udos86.de>
2016-08-09 19:47:56 +0200

    DynamicFormOptionControlModel refactored

--- modules/core/src/model/dynamic-option-control.model.ts
+++ modules/core/src/model/dynamic-option-control.model.ts
@@ -1,29 +1,49 @@
 import {ClsConfig} from "./dynamic-form-control.model";
 import {DynamicFormValueControlModel, DynamicFormValueControlModelConfig} from "./dynamic-form-value-control.model";
 import {getValue} from "../utils";
-import {} from "./dynamic-form-value-control.model";
 
-export interface DynamicFormOption<T> {
+export interface DynamicFormOptionConfig {
 
     disabled?: boolean;
     label?: string;
-    value: T;
-    //selected?: boolean;
+    value: any;
+}
+
+export class DynamicFormOption implements DynamicFormOptionConfig {
+
+    disabled: boolean;
+    label: string;
+    value: any;
+
+    constructor(config: DynamicFormOptionConfig) {
+
+        this.disabled = getValue(config, "disabled", false);
+        this.label = getValue(config, "label", null);
+        this.value = config.value;
+    }
+
+    get text() {
+        return this.label;
+    }
+
+    set text(text: string) {
+        this.label = text;
+    }
 }
 
 export interface DynamicOptionControlModelConfig extends DynamicFormValueControlModelConfig {
 
-    options?: Array<DynamicFormOption<any>>;
+    options?: Array<DynamicFormOptionConfig>;
 }
 
 export abstract class DynamicOptionControlModel<T> extends DynamicFormValueControlModel<T> {
 
-    options: Array<DynamicFormOption<T>>;
+    options: Array<DynamicFormOption>;
 
     constructor(config: DynamicOptionControlModelConfig, cls?: ClsConfig) {
 
         super(config, cls);
-        
-        this.options = getValue(config, "options", []);
+
+        this.options = config.options ? config.options.map(optionConfig => new DynamicFormOption(optionConfig)) : [];
     }
 }
