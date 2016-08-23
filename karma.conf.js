179f76198eef51b37b61dc280d088950916d9c78
Udo Schöfer <developer@udos86.de>
2016-08-23 19:51:38 +0200

    minor updates

--- karma.conf.js
+++ karma.conf.js
@@ -17,42 +17,36 @@ module.exports = function (config) {
 
             'node_modules/core-js/client/shim.min.js',
             'node_modules/reflect-metadata/Reflect.js',
+            'node_modules/zone.js/dist/zone.js',
 
             'node_modules/systemjs/dist/system-polyfills.js',
             'node_modules/systemjs/dist/system.src.js',
 
-            'node_modules/zone.js/dist/zone.js',
-            'node_modules/zone.js/dist/sync-test.js',
-            'node_modules/zone.js/dist/async-test.js',
-            'node_modules/zone.js/dist/fake-async-test.js',
-            'node_modules/zone.js/dist/proxy.js',
-            'node_modules/zone.js/dist/jasmine-patch.js',
-
-            {pattern: 'node_modules/symbol-observable/**/*.js', included: false, watched: true},
+            {pattern: 'node_modules/symbol-observable/**/*.js', included: false, watched: false},
 
             {pattern: 'node_modules/rxjs/**/*.js', included: false, watched: false},
             {pattern: 'node_modules/rxjs/**/*.js.map', included: false, watched: false},
-
-            {pattern: 'karma-test-shim.js', included: true, watched: true},
             
-            {pattern: 'node_modules/@angular/**/*.js', included: false, watched: true},
-            {pattern: 'node_modules/@angular/**/*.js.map', included: false, watched: true},
+            {pattern: 'node_modules/@angular/**/*.js', included: false, watched: false},
+            {pattern: 'node_modules/@angular/**/*.js.map', included: false, watched: false},
 
-            {pattern: 'node_modules/@angular2-material/**/*.js', included: false, watched: true},
-            {pattern: 'node_modules/@angular2-material/**/*.js.map', included: false, watched: true},
+            {pattern: 'node_modules/@angular2-material/**/*.js', included: false, watched: false},
+            {pattern: 'node_modules/@angular2-material/**/*.js.map', included: false, watched: false},
 
-            {pattern: 'node_modules/primeng/**/*.js', included: false, watched: true},
-            {pattern: 'node_modules/primeng/**/*.js.map', included: false, watched: true},
+            {pattern: 'node_modules/primeng/**/*.js', included: false, watched: false},
+            {pattern: 'node_modules/primeng/**/*.js.map', included: false, watched: false},
             
-            {pattern: 'node_modules/@ng2-dynamic-forms/**/*.js', included: false, watched: true},
-            {pattern: 'node_modules/@ng2-dynamic-forms/**/*.js.map', included: false, watched: true},
+            {pattern: 'node_modules/@ng2-dynamic-forms/**/*.js', included: false, watched: false},
+            {pattern: 'node_modules/@ng2-dynamic-forms/**/*.js.map', included: false, watched: false},
 
             {pattern: 'modules/core/src/**/*.js', included: false, watched: true},
             {pattern: 'modules/ui-basic/src/**/*.js', included: false, watched: true},
             {pattern: 'modules/ui-bootstrap/src/**/*.js', included: false, watched: true},
             {pattern: 'modules/ui-foundation/src/**/*.js', included: false, watched: true},
             {pattern: 'modules/ui-material/src/**/*.js', included: false, watched: true},
-            {pattern: 'modules/ui-primeng/src/**/*.js', included: false, watched: true}
+            {pattern: 'modules/ui-primeng/src/**/*.js', included: false, watched: true},
+
+            'karma-test-shim.js'
         ],
 
 
