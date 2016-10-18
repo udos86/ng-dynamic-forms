8f7da399aaf884c2931f34a84c9ba99e299a042c
Udo Schöfer <developer@udos86.de>
2016-10-18 20:20:06 +0200

    Kendo sample excluded

--- example/app/app.module.ts
+++ example/app/app.module.ts
@@ -9,14 +9,14 @@ import {DynamicFormsCoreModule} from "@ng2-dynamic-forms/core";
 import {DynamicFormsBasicUIModule} from "@ng2-dynamic-forms/ui-basic";
 import {DynamicFormsBootstrapUIModule} from "@ng2-dynamic-forms/ui-bootstrap";
 import {DynamicFormsFoundationUIModule} from "@ng2-dynamic-forms/ui-foundation";
-import {DynamicFormsKendoUIModule} from "@ng2-dynamic-forms/ui-kendo";
+//import {DynamicFormsKendoUIModule} from "@ng2-dynamic-forms/ui-kendo";
 import {DynamicFormsMaterialUIModule} from "@ng2-dynamic-forms/ui-material";
 import {DynamicFormsPrimeNGUIModule} from "@ng2-dynamic-forms/ui-primeng";
 
 import {BasicExampleComponent} from "./basic/basic-example.component";
 import {BootstrapExampleComponent} from "./bootstrap/bootstrap-example.component";
 import {FoundationExampleComponent} from "./foundation/foundation-example.component";
-import {KendoExampleComponent} from "./kendo/kendo-example.component";
+//import {KendoExampleComponent} from "./kendo/kendo-example.component";
 import {MaterialExampleComponent} from "./material/material-example.component";
 import {PrimeNGExampleComponent} from "./primeng/primeng-example.component";
 
@@ -34,7 +34,7 @@ import {AppComponent} from './app.component';
         DynamicFormsBasicUIModule,
         DynamicFormsBootstrapUIModule,
         DynamicFormsFoundationUIModule,
-        DynamicFormsKendoUIModule,
+        //DynamicFormsKendoUIModule,
         DynamicFormsMaterialUIModule,
         DynamicFormsPrimeNGUIModule,
         MaterialModule.forRoot()
@@ -43,7 +43,7 @@ import {AppComponent} from './app.component';
         BasicExampleComponent,
         BootstrapExampleComponent,
         FoundationExampleComponent,
-        KendoExampleComponent,
+        //KendoExampleComponent,
         MaterialExampleComponent,
         PrimeNGExampleComponent,
         AppComponent
