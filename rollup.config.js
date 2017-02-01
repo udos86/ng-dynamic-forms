2039e4bd49c871bc4fd32cf9e485853428666c75
Udo Schöfer <developer@udos86.de>
2017-02-01 23:46:14 +0100

    AOT optimizations (#259)

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import uglify from "rollup-plugin-uglify";
 
 export default {
 
-    entry: "example/app/main.jit.js",
+    entry: "example/app/main.aot.js",
 
     dest: "example/dist/bundle.js", // output a single application bundle
 
@@ -13,23 +13,7 @@ export default {
 
     format: "iife",
 
-    onwarn: function (warning) {
-
-        // Skip certain warnings
-
-        // should intercept ... but doesn't in some rollup versions
-        if (warning.code === "THIS_IS_UNDEFINED") {
-            return;
-        }
-
-        // intercepts in some rollup versions
-        if (warning.indexOf("The 'this' keyword is equivalent to 'undefined'") > -1) {
-            return;
-        }
-
-        // console.warn everything else
-        console.warn(warning.message);
-    },
+    onwarn: function (warning) {},
 
     plugins: [
         nodeResolve({
