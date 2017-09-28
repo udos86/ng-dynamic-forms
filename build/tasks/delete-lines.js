27eea1161542757c833d3941dadb1631c3a29beb
Udo Schöfer <developer@udos86.de>
2017-09-28 20:08:29 +0200

    1.4.32

--- build/tasks/delete-lines.js
+++ build/tasks/delete-lines.js
@@ -1,12 +1,12 @@
 const gulp        = require("gulp"),
       deleteLines = require("gulp-delete-lines");
 
-module.exports = function (src, dest) {
+module.exports = function (src, dest, filters) {
 
     return function () {
 
         return gulp.src(src)
-                   .pipe(deleteLines({"filters": [/moduleId: module.id/]}))
+                   .pipe(deleteLines({"filters": filters}))
                    .pipe(gulp.dest(dest));
     };
 };
