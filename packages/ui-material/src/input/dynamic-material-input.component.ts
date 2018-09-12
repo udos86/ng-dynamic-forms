693d4a01b0cbd83213abe286fccc546842999716
Udo Schöfer <developer@udos86.de>
2018-09-12 14:59:56 +0200

    ErrorStateMatcher support for Material

--- packages/ui-material/src/input/dynamic-material-input.component.ts
+++ packages/ui-material/src/input/dynamic-material-input.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Inject, Input, Optional, Output, ViewChild } from "@angular/core";
 import { FormGroup } from "@angular/forms";
 import {
+    ErrorStateMatcher,
     LabelOptions,
     MAT_AUTOCOMPLETE_DEFAULT_OPTIONS,
     MAT_LABEL_GLOBAL_OPTIONS,
@@ -40,6 +41,7 @@ export class DynamicMaterialInputComponent extends DynamicMaterialFormInputContr
 
     constructor(protected layoutService: DynamicFormLayoutService,
                 protected validationService: DynamicFormValidationService,
+                @Inject(ErrorStateMatcher) public errorStateMatcher: ErrorStateMatcher,
                 @Inject(MAT_AUTOCOMPLETE_DEFAULT_OPTIONS) public AUTOCOMPLETE_OPTIONS: MatAutocompleteDefaultOptions,
                 @Inject(MAT_LABEL_GLOBAL_OPTIONS) @Optional() public LABEL_OPTIONS: LabelOptions,
                 @Inject(MAT_RIPPLE_GLOBAL_OPTIONS) @Optional() public RIPPLE_OPTIONS: RippleGlobalOptions) {
