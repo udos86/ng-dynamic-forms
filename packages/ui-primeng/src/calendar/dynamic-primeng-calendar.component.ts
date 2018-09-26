f3ff7bea42285838c5638707d799558e59441a7f
Dhruv Bhavsar <dhruvb@microsoft.com>
2018-09-26 11:25:28 -0400

    remove unused import

--- packages/ui-primeng/src/calendar/dynamic-primeng-calendar.component.ts
+++ packages/ui-primeng/src/calendar/dynamic-primeng-calendar.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output, ViewChild } from "@angular/core";
 import { FormGroup } from "@angular/forms";
-import { Calendar, Tooltip } from "primeng/primeng";
+import { Calendar } from "primeng/primeng";
 import {
     DynamicDatePickerModel, 
     DynamicFormControlCustomEvent,
