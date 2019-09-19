f874c00bfee5995f9b04e5bf5d396098e3c476ae
Udo Schöfer <developer@udos86.de>
2019-09-19 11:39:01 +0200

    Switch control added to NG Bootstrap

---
+++ projects/ng-dynamic-forms/ui-ng-bootstrap/src/lib/switch/dynamic-ng-bootstrap-switch.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { FormGroup } from "@angular/forms";
+import {
+    DynamicFormControlComponent,
+    DynamicFormLayout,
+    DynamicFormLayoutService,
+    DynamicFormValidationService,
+    DynamicSwitchModel
+} from "@ng-dynamic-forms/core";
+
+@Component({
+    selector: "dynamic-ng-bootstrap-switch",
+    templateUrl: "./dynamic-ng-bootstrap-switch.component.html"
+})
+export class DynamicNGBootstrapSwitchComponent extends DynamicFormControlComponent {
+
+    @Input() group: FormGroup;
+    @Input() layout: DynamicFormLayout;
+    @Input() model: DynamicSwitchModel;
+
+    @Output() blur: EventEmitter<any> = new EventEmitter();
+    @Output() change: EventEmitter<any> = new EventEmitter();
+    @Output() focus: EventEmitter<any> = new EventEmitter();
+
+    constructor(protected layoutService: DynamicFormLayoutService,
+                protected validationService: DynamicFormValidationService) {
+
+        super(layoutService, validationService);
+    }
+}
