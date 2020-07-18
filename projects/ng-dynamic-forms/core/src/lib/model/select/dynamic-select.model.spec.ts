922d41580fccb935f2b4124ebc2388faf104ee36
Udo Schöfer <developer@udos86.de>
2020-07-18 12:04:52 +0200

    12.0.0 RC

--- projects/ng-dynamic-forms/core/src/lib/model/select/dynamic-select.model.spec.ts
+++ projects/ng-dynamic-forms/core/src/lib/model/select/dynamic-select.model.spec.ts
@@ -1,7 +1,6 @@
 import { async } from "@angular/core/testing";
 import { Observable } from "rxjs";
 import { DYNAMIC_FORM_CONTROL_TYPE_SELECT, DynamicSelectModel } from "./dynamic-select.model";
-import { ɵlooseIdentical as looseIdentical } from "@angular/core";
 
 describe("DynamicSelectModel test suite", () => {
 
@@ -36,7 +35,7 @@ describe("DynamicSelectModel test suite", () => {
         expect(model.placeholder).toEqual("");
         expect(model.type).toEqual(DYNAMIC_FORM_CONTROL_TYPE_SELECT);
         expect(model.value).toBeNull();
-        expect(model.compareWithFn).toBe(looseIdentical);
+        expect(model.compareWithFn).toBe(Object.is);
         expect(model.disabledChanges).toBeDefined();
         expect(model.valueChanges).toBeDefined();
     });
