00fb68b488ac9aaa9be147097893349c127ef4fa
Udo Schöfer <developer@udos86.de>
2022-12-02 21:41:03 +0100

    updates to Angular 15

--- projects/ng-dynamic-forms/ui-ng-bootstrap/src/test.ts
+++ projects/ng-dynamic-forms/ui-ng-bootstrap/src/test.ts
@@ -10,8 +10,6 @@ import {
   platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
 
-declare const require: any;
-
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
@@ -19,7 +17,3 @@ getTestBed().initTestEnvironment(
     teardown: { destroyAfterEach: false }
 }
 );
-// Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
-// And load the modules.
-context.keys().map(context);
